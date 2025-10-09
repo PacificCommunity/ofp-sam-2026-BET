e7dcaae465227a2791ee82f2103e5ac103d45fc6
kyuhank <kh2064@gmail.com>
2025-10-09 12:29:25 +1100

    configs test

--- configs/test.R
+++ configs/test.R
@@ -1,28 +1,28 @@
 
 
 models=list("base"=list(mfcl_commands=paste("bet.frq 11.par 12.par",
-                                             "-switch 1 1 1 10",
+                                             "-switch 1 1 1 1",
                                              sep = " ")),
             
             "m1"=list(mfcl_commands=paste("bet.frq 11.par 12.par",
-                                           "-switch 1 1 1 20",
+                                           "-switch 1 1 1 1",
                                            sep = " ")),
             
             "m2"=list(mfcl_commands=paste("bet.frq 11.par 12.par",
-                                           "-switch 1 1 1 30",
+                                           "-switch 1 1 1 1",
                                            sep = " "))
             )
 
 
 program_path <- "../../mfcl/exe/mfclo64_2023"
-base_dir <- "base/2023"
+base_dir <- "mfcl/inputs/2023"
 
 ### post-processing 
 
 ModelIDs <- names(models)
 models <- Map(function(x, nm) {
   x$mfcl_commands <- paste(program_path, x$mfcl_commands)
-  x$model_dir <- paste0("mfcl/",nm)
+  x$model_dir <- paste0("model/",nm)
   x$base_dir <- base_dir
   x$program_path <- program_path
   x
