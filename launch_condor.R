faf8ee2def0421b728aa94473839f03fb1130e12
kyuhank <kh2064@gmail.com>
2026-01-25 12:01:43 +1100

    doitall.sh

--- launch_condor.R
+++ launch_condor.R
@@ -24,7 +24,7 @@ branch <- "develop"                                              # Branch of git
 setwd(here::here())
 
 #dir="develop/likelihood_test6" 
-dir="develop/Jan_25_doitall_par"
+dir="develop/Jan_25_doitallsh_par"
 
 make="run"
 
