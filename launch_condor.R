02ece473d6f7ade96c819a592ee8611f6ea3b101
kyuhank <kh2064@gmail.com>
2026-01-29 19:58:55 +1100

    mixing periods test

--- launch_condor.R
+++ launch_condor.R
@@ -24,7 +24,7 @@ branch <- "develop"                                              # Branch of git
 setwd(here::here())
 
 #dir="develop/likelihood_test6" 
-dir="develop/Jan_29_likTest"
+dir="develop/Jan_29_mixp"
 
 make="run"
 
