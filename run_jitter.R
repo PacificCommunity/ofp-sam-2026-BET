3e144f1f7d1bc88679f549e34d897a45c2507cda
kyuhank <kh2064@gmail.com>
2026-02-02 19:01:22 +1100

    increased eval

--- run_jitter.R
+++ run_jitter.R
@@ -82,7 +82,7 @@ cat("Jittered par file written:", jittered_par_name, "\n")
 ##############
 
 defaultswitch <- paste("-switch 1",
-                       "1 1 1000",
+                       "1 1 5000",
                        sep=" ")
 
 output_par_name <- paste0("jittered_out_", jitter_seed, ".par")
