f8113ede929948e4caf98161256160ff3d2a2d0b
kyuhank <kh2064@gmail.com>
2026-02-05 01:01:56 +1100

    updated functions in flr4mfcl

--- tools/retro.R
+++ tools/retro.R
@@ -94,8 +94,7 @@ retro.ini <- function(ini.obj, tag.obj, max_year) {
 
 
 
-read.MFCLIni<-function (inifile, nseasons = 4) 
-{
+read.MFCLIni<-function (inifile, nseasons = 4) {
   trim.leading <- function(x) sub("^\\s+", "", x)
   trim.trailing <- function(x) sub("\\s+$", "", x)
   splitter <- function(ff, tt, ll = 1, inst = 1) unlist(strsplit(trim.leading(ff[grep(tt, 
@@ -158,5 +157,161 @@ read.MFCLIni<-function (inifile, nseasons = 4)
                                seasons = dim(slot(res, "mat"))[4], regions = nregions, 
                                fisheries = dim(slot(res, "tag_fish_rep_rate"))[2], taggrps = dim(slot(res, 
                                                                                                       "tag_fish_rep_rate"))[1] - 1)
+  return(res)
+}
+
+
+
+
+
+read.MFCLTagRep<-function (parfile, parobj = NULL, first.yr = NA) {
+  trim.leading <- function(x) sub("^\\s+", "", x)
+  splitter <- function(ff, tt, ll = 1) unlist(strsplit(trim.leading(ff[grep(tt, 
+                                                                            ff)[1] + ll]), split = "[[:blank:]]+"))
+  nlines <- function(ff, tt) grep("#", ff)[grep("#", ff) > 
+                                             grep(tt, ff)[1]][1] - grep(tt, ff)[1] - 1
+  res <- new("MFCLTagRep")
+  if (is.null(parobj)) {
+    par <- readLines(parfile)
+    par <- par[nchar(par) >= 1]
+    if (any(grepl("# ", par) & nchar(par) < 3)) 
+      par <- par[-seq(1, length(par))[grepl("# ", par) & 
+                                        nchar(par) < 3]]
+  }
+  if (!is.null(parobj)) 
+    par <- parobj
+  if (length(grep("# tag fish rep", par)) == 0) 
+    return(res)
+  vsn <- as.numeric(unlist(strsplit(trimws(par[2]), split = "[[:blank:]]+")))[200]
+  nfish <- length(splitter(par, "# tag fish rep", 1))
+  nfish_rrdc <- grep("# availability coffs", par) - grep("# Reporting rate dev coffs", 
+                                                         par) - 1
+  if (vsn >= 1065 & length(grep("tagmort", par)) > 0) 
+    slot(res, "tag_shed_rate") <- as.numeric(splitter(par, 
+                                                      "# tagmort"))
+  slot(res, "tag_fish_rep_rate") <- t(array(as.numeric(splitter(par, 
+                                                                "# tag fish rep", 1:(nlines(par, "# tag fish rep")))), 
+                                            dim = c(nfish, nlines(par, "# tag fish rep"))))
+  slot(res, "tag_fish_rep_grp") <- t(array(as.numeric(splitter(par, 
+                                                               "# tag fish rep group flags", 1:(nlines(par, "# tag fish rep group flags")))), 
+                                           dim = c(nfish, nlines(par, "# tag fish rep group flags"))))
+  slot(res, "tag_fish_rep_flags") <- t(array(as.numeric(splitter(par, 
+                                                                 "# tag_fish_rep active flags", 1:(nlines(par, "# tag_fish_rep active flags")))), 
+                                             dim = c(nfish, nlines(par, "# tag_fish_rep active flags"))))
+  slot(res, "tag_fish_rep_target") <- t(array(as.numeric(splitter(par, 
+                                                                  "# tag_fish_rep target", 1:(nlines(par, "# tag_fish_rep target")))), 
+                                              dim = c(nfish, nlines(par, "# tag_fish_rep target"))))
+  slot(res, "tag_fish_rep_pen") <- t(array(as.numeric(splitter(par, 
+                                                               "# tag_fish_rep penalty", 1:(nlines(par, "# tag_fish_rep penalty")))), 
+                                           dim = c(nfish, nlines(par, "# tag_fish_rep penalty"))))
+  if (length(nfish_rrdc) > 0) 
+    slot(res, "rep_rate_dev_coffs") <- lapply(seq(1:nfish_rrdc), 
+                                              function(x) as.numeric(splitter(par, "# Reporting rate dev coffs", 
+                                                                              x)))
+  return(res)
+}
+
+
+
+read.MFCLSel<-function (parfile, parobj = NULL, first.yr = NA) {
+  trim.leading <- function(x) sub("^\\s+", "", x)
+  trim.trailing <- function(x) sub("\\s+$", "", x)
+  splitter <- function(ff, tt, ll = 1, inst = 1) unlist(strsplit(trim.leading(ff[grep(tt, 
+                                                                                      ff)[inst] + ll]), split = "[[:blank:]]+"))
+  res <- new("MFCLSel")
+  if (is.null(parobj)) {
+    par <- trim.leading(readLines(parfile))
+    par <- par[nchar(par) >= 1]
+    par <- par[-seq(1, length(par))[grepl("# ", par) & nchar(par) < 
+                                      3]]
+  }
+  if (!is.null(parobj)) 
+    par <- parobj
+  parversion <- as.numeric(splitter(par, "# The parest_flags"))[200]
+  nseasons <- length(splitter(par, "# season_flags"))
+  nregions <- length(splitter(par, "# region parameters"))
+  nagecls <- as.numeric(par[grep("# The number of age classes", 
+                                 par) + 1])
+  nfish <- length(splitter(par, "# q0_miss"))
+  xfish <- sum(matrix(as.numeric(splitter(par, "# fish flags", 
+                                          ll = 1:nfish, inst = 1)), ncol = nfish)[71, ])
+  qfish <- grep("# selectivity deviation coefficients", par) - 
+    grep("# catchability deviation coefficients", par) - 
+    1
+  nqgroups <- max(array(as.numeric(splitter(par, "# fish flags", 
+                                            1:nfish)), dim = c(100, nfish))[29, ], na.rm = T)
+  dims1 <- list(age = as.character(seq(0, (nagecls/nseasons) - 
+                                         1)), year = "all", unit = "unique", season = c(as.character(1:nseasons)), 
+                area = "all")
+  dims2 <- list(age = as.character(seq(0, (nagecls/nseasons) - 
+                                         1)), year = "all", unit = c(as.character(1:nfish)), season = c(as.character(1:nseasons)), 
+                area = "all")
+  dims2a <- dims2
+  dims2a$unit <- as.character(1:(nfish + xfish))
+  dims3 <- list(age = "all", year = "all", unit = as.character(1:nfish), 
+                season = "all", area = "all")
+  qdc <- lapply(1:qfish, function(x) as.numeric(splitter(par, 
+                                                         "# catchability deviation coefficients", x)))
+  edc <- lapply(1:nfish, function(x) as.numeric(splitter(par, 
+                                                         "# effort deviation coefficients", x)))
+  cdc <- lapply(1:nqgroups, function(x) as.numeric(splitter(par, 
+                                                            "# The grouped_catch_dev_coffs", x, inst = 2)))
+  sdc_end <- cumsum(lapply(edc, length))
+  sdc_start <- c(1, sdc_end[-length(sdc_end)] + 1)
+  sdc_lines <- lapply(seq(length(sdc_end)), function(x) seq(sdc_start[x], 
+                                                            sdc_end[x]))
+  sdc <- lapply(sdc_lines, function(x) matrix(as.numeric(splitter(par, 
+                                                                  "# sel_dev_coffs", x)), ncol = nagecls))
+  slot(res, "availability_coffs") <- FLQuant(aperm(array(as.numeric(splitter(par, 
+                                                                             "# availability coffs")), dim = c(nseasons, nagecls/nseasons, 
+                                                                                                               1, 1, 1)), c(2, 3, 4, 1, 5)), dimnames = dims1)
+  slot(res, "fishery_sel") <- FLQuant(aperm(array(as.numeric(splitter(par, 
+                                                                      "# fishery selectivity", 1:(nfish + xfish))), dim = c(nseasons, 
+                                                                                                                            nagecls/nseasons, nfish + xfish, 1, 1)), c(2, 4, 3, 1, 
+                                                                                                                                                                       5)), dimnames = dims2a)
+  slot(res, "fishery_sel_age_comp") <- FLQuant(aperm(array(as.numeric(splitter(par, 
+                                                                               "# age-dependent component of fishery selectivity", 1:nfish)), 
+                                                           dim = c(nseasons, nagecls/nseasons, nfish, 1, 1)), c(2, 
+                                                                                                                4, 3, 1, 5)), dimnames = dims2)
+  slot(res, "av_q_coffs") <- FLQuant(aperm(array(as.numeric(splitter(par, 
+                                                                     "# average catchability coefficients")), dim = c(1, 1, 
+                                                                                                                      1, nfish, 1)), c(2, 3, 4, 1, 5)), dimnames = dims3)
+  slot(res, "ini_q_coffs") <- FLQuant(aperm(array(as.numeric(splitter(par, 
+                                                                      "# initial trend in catchability coefficients")), dim = c(nfish, 
+                                                                                                                                1, 1, 1, 1)), c(2, 3, 1, 4, 5)), dimnames = dims3)
+  slot(res, "q0_miss") <- FLQuant(aperm(array(as.numeric(splitter(par, 
+                                                                  "# q0_miss")), dim = c(nfish, 1, 1, 1, 1)), c(2, 3, 1, 
+                                                                                                                4, 5)), dimnames = dims3)
+  slot(res, "sel_dev_corr") <- FLQuant(aperm(array(as.numeric(splitter(par, 
+                                                                       "# correlation in selectivity deviations")), dim = c(nfish, 
+                                                                                                                            1, 1, 1, 1)), c(2, 3, 1, 4, 5)), dimnames = dims3)
+  slot(res, "season_q_pars") <- matrix(as.numeric(splitter(par, 
+                                                           "# seasonal_catchability_pars", 1:nfish)), ncol = 12, 
+                                       byrow = T)
+  slot(res, "q_dev_coffs") <- qdc
+  slot(res, "effort_dev_coffs") <- edc
+  slot(res, "catch_dev_coffs") <- cdc
+  slot(res, "catch_dev_coffs_flag") <- as.numeric(par[grep("# The grouped_catch_dev_coffs flag", 
+                                                           par) + 1])
+  slot(res, "sel_dev_coffs") <- matrix(as.numeric(splitter(par, 
+                                                           "# selectivity deviation coefficients", 1:sum(unlist(lapply(qdc, 
+                                                                                                                       length))))), ncol = nagecls, byrow = T)
+  slot(res, "sel_dev_coffs2") <- sdc
+  getfishparms <- function(xx, vsn) {
+    if (vsn > 1040 & vsn <= 1049) 
+      mm <- matrix(as.numeric(splitter(xx, "# extra fishery parameters", 
+                                       1:20)), ncol = nfish, byrow = T)
+    if (vsn >= 1050) 
+      mm <- matrix(as.numeric(splitter(xx, "# extra fishery parameters", 
+                                       1:50)), ncol = nfish, byrow = T)
+    return(mm)
+  }
+  slot(res, "fish_params") <- getfishparms(par, parversion)
+  if (parversion >= 1052) 
+    slot(res, "spp_params") <- matrix(as.numeric(splitter(par, 
+                                                          "# species parameters", 1:20)), ncol = 1, byrow = T)
+  slot(res, "range") <- c(min = 0, max = nagecls/nseasons, 
+                          plusgroup = NA, minyear = 1, maxyear = 1)
+  res <- checkUnitDimnames(res, nfisheries = nfish)
   return(res)
 }
