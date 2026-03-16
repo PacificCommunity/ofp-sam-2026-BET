c08d802e5d0ad2e9dfaf7ec67e7e72070a13d891
kyuhank <kh2064@gmail.com>
2026-03-16 12:50:29 +1100

    configs for M and growth sens

---
+++ configs/2023NatGrowthSens.R
@@ -0,0 +1,128 @@
+summary <-"Investigating the sensitivity of model stability to M and growth parameters"
+
+source("../tools/model_defaults.R")
+
+models <- list(
+  
+  "2023_fixM" = list(
+    
+    "description"="Fixing M-at-age at the values from the 2023 diagnostic model",
+    
+    mfcl_commands ="./doitall.sh",
+    program_path = "mfcl/exe/mfclo64_2026_02_04_vsn2278",  # Model-specific path
+    base_dir = "mfcl/inputs/2023_fixM",                   # Model-specific dir
+    
+    ## configuration for profiling
+    Reps = "15 25 25 300 300 300",
+    scalars = paste0((seq(150, 50, by=-5)), collapse = " "),
+    Af172 = "0",
+    Af173 = "0",
+    Af174 = "0",
+    
+    ## retrospective configuration
+    retro_peels = "1 2 3 4 5 6 7",
+    
+    
+    ## n_mixing_periods (this is only for retrospective runs, so should match with what is specified in doitall.sh)
+    n_mixing_periods = "2",
+    
+    ## min_year
+    min_year= "1952",
+    
+    ## Jitter settings
+    jitter_seeds = paste0(1:50, collapse = " "),
+    jitter_cv = "0.2",
+    
+    ## post-run hessian toggles
+    jitter_hessian = "0",
+    model_hessian = "0",
+    prof_hessian = "0",
+    retro_hessian = "0",
+    
+    ## hessian parallel settings
+    nsplit="5"
+  ),
+  
+  "2023_fixVB" = list(
+    
+    "description"="Fixing growth parameters at the values from the 2023 diagnostic model",
+    
+    mfcl_commands ="./doitall.sh",
+    program_path = "mfcl/exe/mfclo64_2026_02_04_vsn2278",  # Model-specific path
+    base_dir = "mfcl/inputs/2023_fixVB",                   # Model-specific dir
+    
+    ## configuration for profiling
+    Reps = "15 25 25 300 300 300",
+    scalars = paste0((seq(150, 50, by=-5)), collapse = " "),
+    Af172 = "0",
+    Af173 = "0",
+    Af174 = "0",
+    
+    ## retrospective configuration
+    retro_peels = "1 2 3 4 5 6 7",
+    
+    
+    ## n_mixing_periods (this is only for retrospective runs, so should match with what is specified in doitall.sh)
+    n_mixing_periods = "2",
+    
+    ## min_year
+    min_year= "1952",
+    
+    ## Jitter settings
+    jitter_seeds = paste0(1:50, collapse = " "),
+    jitter_cv = "0.2",
+    
+    ## post-run hessian toggles
+    jitter_hessian = "0",
+    model_hessian = "0",
+    prof_hessian = "0",
+    retro_hessian = "0",
+    
+    ## hessian parallel settings
+    nsplit="5"
+  ),
+  
+  "2023_fixVB_M" = list(
+    
+    "description"="Fixing both M-at-age and growth parameters at the values from the 2023 diagnostic model",
+    
+    mfcl_commands ="./doitall.sh",
+    program_path = "mfcl/exe/mfclo64_2026_02_04_vsn2278",  # Model-specific path
+    base_dir = "mfcl/inputs/2023_fixVB_M",                   # Model-specific dir
+    
+    ## configuration for profiling
+    Reps = "15 25 25 300 300 300",
+    scalars = paste0((seq(150, 50, by=-5)), collapse = " "),
+    Af172 = "0",
+    Af173 = "0",
+    Af174 = "0",
+    
+    ## retrospective configuration
+    retro_peels = "1 2 3 4 5 6 7",
+    
+    
+    ## n_mixing_periods (this is only for retrospective runs, so should match with what is specified in doitall.sh)
+    n_mixing_periods = "2",
+    
+    ## min_year
+    min_year= "1952",
+    
+    ## Jitter settings
+    jitter_seeds = paste0(1:50, collapse = " "),
+    jitter_cv = "0.2",
+    
+    ## post-run hessian toggles
+    jitter_hessian = "0",
+    model_hessian = "0",
+    prof_hessian = "0",
+    retro_hessian = "0",
+    
+    ## hessian parallel settings
+    nsplit="5"
+  )
+  
+  )
+
+
+models <- apply_model_defaults(models)
+ModelIDs <- names(models)
